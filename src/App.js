47460cfced07820fe720cf5dc95cc36607bee634
MegNia2 <165702683+MegNia2@users.noreply.github.com>
2024-09-20 20:43:10 +0100

    imported bootstrap

--- src/App.js
+++ src/App.js
@@ -2,18 +2,24 @@ export default function App() {
   return (
     <div>
       <h1>My Weather App</h1>
-      <footer>
-        <p>
-          Coded by Megan Gee, and is open-sourced on{" "}
-          <a href="https://github.com/MegNia2/weather-app-v3" target="_blank">
-            Github
-          </a>{" "}
-          hosted on{" "}
-          <a href="https://statuesque-faun-890912.netlify.app/" target="_blank">
-            Netlify
-          </a>
-        </p>
-      </footer>
+      <p>
+        Coded by Megan Gee, and is open-sourced on{" "}
+        <a
+          href="https://github.com/MegNia2/weather-app-v3"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>{" "}
+        hosted on{" "}
+        <a
+          href="https://statuesque-faun-890912.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </p>
     </div>
   );
 }
