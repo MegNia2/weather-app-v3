29f8d95cd047c23566d0ec876ae33007aed92f68
MegNia2 <165702683+MegNia2@users.noreply.github.com>
2024-09-20 20:12:53 +0100

    Added links to Github and Netlify

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ export default function App() {
     <div>
       <h1>My Weather App</h1>
       <footer>
-        <p>Coded by Megan Gee, and is open-sourced on Github</p>
+        <p>
+          Coded by Megan Gee, and is open-sourced on{" "}
+          <a href="https://github.com/MegNia2/weather-app-v3" target="_blank">
+            Github
+          </a>{" "}
+          hosted on{" "}
+          <a href="https://statuesque-faun-890912.netlify.app/" target="_blank">
+            Netlify
+          </a>
+        </p>
       </footer>
     </div>
   );
