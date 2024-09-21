54a5f12e3196d68d9c462d870c68423d1712e455
MegNia2 <165702683+MegNia2@users.noreply.github.com>
2024-09-21 08:11:09 +0100

    Updated css of weather app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import Weather from "./Weather";
+import "./App.css";
 
 export default function App() {
   return (
-    <div>
-      <h1>My Weather App</h1>
-      <Weather />
-      <p>
-        Coded by Megan Gee, and is open-sourced on{" "}
-        <a
-          href="https://github.com/MegNia2/weather-app-v3"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Github
-        </a>{" "}
-        hosted on{" "}
-        <a
-          href="https://statuesque-faun-890912.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Netlify
-        </a>
-      </p>
+    <div className="App">
+      <div className="container">
+        <Weather />
+        <footer>
+          Coded by Megan Gee, and is open-sourced on{" "}
+          <a
+            href="https://github.com/MegNia2/weather-app-v3"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>{" "}
+          hosted on{" "}
+          <a
+            href="https://statuesque-faun-890912.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -3,34 +3,37 @@ import "./Weather.css";
 
 export default function Weather() {
   return (
-    <div>
+    <div className="Weather">
       <form>
-        <input type="search"></input>
-        <input type="submit"></input>
+        <div className="row">
+          <div className="col-9">
+            <input type="search" className="form-control"></input>
+          </div>
+
+          <div className="col-3">
+            <input type="submit" className="btn btn-primary w-100"></input>
+          </div>
+        </div>
       </form>
       <h3>London</h3>
+      <p>Friday, Clear</p>
       <div className="row">
-        <div className="col-3">
-          <div className="temperature">
-            <div>
-              <img
-                src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
-                alt="sun-icon"
-              />
-            </div>
-            <div>18°C</div>
+        <div className="col-6">
+          <div className="d-flex">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+              alt="sun-icon"
+            />
+
+            <span className="temperature">18</span>
+            <span className="celcius">°C</span>
           </div>
+        </div>
+        <div className="col-6">
           <ul>
             <li>Precipitation: 2%</li>
             <li>Humidity: 69%</li>
             <li>Wind: 6mph</li>
-          </ul>
-        </div>
-        <div className="col-9">
-          <h4>Weather</h4>
-          <ul>
-            <li>Friday</li>
-            <li>Clear</li>
           </ul>
         </div>
       </div>
