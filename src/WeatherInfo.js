43af334764e689f08954fd550acfd287ed28107a
MegNia2 <165702683+MegNia2@users.noreply.github.com>
2024-09-24 18:52:07 +0100

    Added the component WeatherInfo to hold weatherData

---
+++ src/WeatherInfo.js
@@ -0,0 +1,32 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <h3>{props.data.city}</h3>
+      <ul>
+        <li>
+          <FormattedDate date={props.data.date} />
+        </li>
+        <li>{props.data.description}</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <div className="d-flex">
+            <img src={props.data.icon} alt={props.data.description} />
+
+            <span className="temp">{Math.round(props.data.temperature)}</span>
+            <span className="celcius">°C</span>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Wind: {Math.round(props.data.wind)}mph</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
