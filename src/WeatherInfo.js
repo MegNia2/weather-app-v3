5b4b890523c8af53af80764de00439e0c51849e8
MegNia2 <165702683+MegNia2@users.noreply.github.com>
2024-09-25 18:53:16 +0100

    Replaced the img with WeatherIcon component

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
   return (
@@ -14,8 +15,7 @@ export default function WeatherInfo(props) {
       <div className="row">
         <div className="col-6">
           <div className="d-flex">
-            <img src={props.data.icon} alt={props.data.description} />
-
+            <WeatherIcon code={props.data.icon} size={42} />
             <span className="temp">{Math.round(props.data.temperature)}</span>
             <span className="celcius">°C</span>
           </div>
