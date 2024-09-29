2468d7315fa45cbf761299fad62cd988ccfb35ce
MegNia2 <165702683+MegNia2@users.noreply.github.com>
2024-09-29 12:17:50 +0100

    Update the size element of the icon to props.size

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -27,7 +27,7 @@ export default function WeatherIcon(props) {
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
       color="#48484A"
-      size={64}
+      size={props.size}
       animate={true}
     />
   );
